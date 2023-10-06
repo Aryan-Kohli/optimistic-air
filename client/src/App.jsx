997a9e072143b2aa972dc365fe5959c491361399
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-10-06 21:07:18 +0530

    four

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Registerpage from "./components/registerpage";
 import { UserContextProvider } from "./UserContext";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000/";
+axios.defaults.baseURL = "https://optimistic-air-backend.onrender.com";
 axios.defaults.withCredentials = true;
 export default function App() {
   const [emotions, setemotions] = React.useState("");
