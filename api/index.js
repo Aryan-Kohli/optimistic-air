644da9994b5a38904c0f422872108c8a16e43dce
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-10-12 06:30:44 +0530

    five

--- api/index.js
+++ api/index.js
@@ -12,12 +12,21 @@ const jwtsecret = "nosecret";
 app.use(express.json());
 const bcryptsalt = bcrypt.genSaltSync(10);
 
+
 app.use(cookieParser());
 mongoose.connect(process.env.MONGO_URL);
 app.use(cors({
     credentials:true,
-    origin:"https://optimistic-air.netlify.app/"
+    origin:"https://optimistic-air.netlify.app"
 }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://optimistic-air.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.post('/register',async (req,resp)=>{
     const {firstname,
         lastname,
