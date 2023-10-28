92eb8ef8bebe558f8936ae88275257da03a3308c
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-10-28 17:58:15 +0530

    vercel

--- api/index.js
+++ api/index.js
@@ -21,12 +21,6 @@ app.use(cors({
     // origin: "http://localhost:5173"
 }));
 app.use((req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  res.redirect(`https://${req.headers.host}${req.url}`);
-});
-app.use((req, res, next) => {
     // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Origin', 'https://optimistic-air.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -57,71 +51,71 @@ app.post('/register',async (req,resp)=>{
     })
     resp.json(userdoc);
 })
-app.post('/login', async (req, resp) => {
-  const { email, password } = req.body;
-  try {
-    const userdoc = await User.findOne({ email: req.body.email });
-    console.log(req.body);
-    if (userdoc) {
-      const comp_pass = bcrypt.compareSync(password, userdoc.password);
-      if (comp_pass) {
-        jwt.sign(
-          {
-            firstname: userdoc.firstname,
-            lastname: userdoc.lastname,
-            email: userdoc.email,
-            location: userdoc.location,
-            id: userdoc._id
-          },
-          jwtsecret,
-          {},
-          (err, token) => {
-            if (err) throw err;
-            resp.cookie('token', token, { secure: true, httpOnly: true, sameSite: 'strict' }).json(userdoc);
-          }
-        );
-      } else {
-        resp.status(400).json("Password is wrong");
-      }
-    } else {
-      resp.status(400).json("Register first");
-    }
-  } catch (e) {
-    console.error(e);
-    resp.status(500).json("Internal Server Error");
-  }
-});
-
-// app.post('/login',async (req,resp)=>{
-//     const {email,password}=req.body;
-//     try{
-
-//         const userdoc = await  User.findOne({email:req.body.email});
-//         console.log(req.body);
-//     if(userdoc)
-//     {
-//         const comp_pass = bcrypt.compareSync(password,userdoc.password);
-//         if(comp_pass)
-//         {
-//             jwt.sign({firstname:userdoc.firstname,lastname:userdoc.lastname,email:userdoc.email,location:userdoc.location,id:userdoc._id},jwtsecret,{},(err,token)=>{
-//                 if(err) throw err;
-//                 resp.cookie('token',token).json(userdoc);
-//             })
-//         }
-//         else{
-//             resp.status.apply(400).json("password is wrong");
-//         }
+// app.post('/login', async (req, resp) => {
+//   const { email, password } = req.body;
+//   try {
+//     const userdoc = await User.findOne({ email: req.body.email });
+//     console.log(req.body);
+//     if (userdoc) {
+//       const comp_pass = bcrypt.compareSync(password, userdoc.password);
+//       if (comp_pass) {
+//         jwt.sign(
+//           {
+//             firstname: userdoc.firstname,
+//             lastname: userdoc.lastname,
+//             email: userdoc.email,
+//             location: userdoc.location,
+//             id: userdoc._id
+//           },
+//           jwtsecret,
+//           {},
+//           (err, token) => {
+//             if (err) throw err;
+//             resp.cookie('token', token, { secure: true, httpOnly: true, sameSite: 'strict' }).json(userdoc);
+//           }
+//         );
+//       } else {
+//         resp.status(400).json("Password is wrong");
+//       }
+//     } else {
+//       resp.status(400).json("Register first");
 //     }
-//     else{
-//         resp.status(400).json("register first");
-//     }
-// }
-// catch(e)
-// {
-//       console.error(e);
+//   } catch (e) {
+//     console.error(e);
 //     resp.status(500).json("Internal Server Error");
-// }
-// })
+//   }
+// });
+
+app.post('/login',async (req,resp)=>{
+    const {email,password}=req.body;
+    try{
+
+        const userdoc = await  User.findOne({email:req.body.email});
+        console.log(req.body);
+    if(userdoc)
+    {
+        const comp_pass = bcrypt.compareSync(password,userdoc.password);
+        if(comp_pass)
+        {
+            jwt.sign({firstname:userdoc.firstname,lastname:userdoc.lastname,email:userdoc.email,location:userdoc.location,id:userdoc._id},jwtsecret,{},(err,token)=>{
+                if(err) throw err;
+                resp.cookie('token',token).json(userdoc);
+            })
+        }
+        else{
+            resp.status.apply(400).json("password is wrong");
+        }
+    }
+    else{
+        resp.status(400).json("register first");
+    }
+}
+catch(e)
+{
+      console.error(e);
+    resp.status(500).json("Internal Server Error");
+}
+})
 app.get('/check',(req,resp)=>{
     const {token}=req.cookies;
     console.log("all god");
