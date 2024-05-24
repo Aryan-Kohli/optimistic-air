7cb1aefcef5955295aa6f8ef0632bc99378bcfd2
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2024-05-25 00:10:25 +0530

    error fixed

--- api/index.js
+++ api/index.js
@@ -22,8 +22,8 @@ app.use(cors({
     // origin: "*"
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    // res.header('Access-Control-Allow-Origin', 'https://optimistic-air.netlify.app');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://optimistic-air.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
